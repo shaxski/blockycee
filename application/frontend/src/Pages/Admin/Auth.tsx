c18a9c3313f8ea1a171bbebb0261e7e54cba0230
Kai Cho <kai.cho@netlogix.co.nz>
2023-10-23 20:40:14 +1300

    add admin authentication dialog

---
+++ application/frontend/src/Pages/Admin/Auth.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Auth() {
+	return (
+		<div>
+		</div>
+	)
+}
--- application/frontend/src/Pages/Home.tsx
+++ application/frontend/src/Pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
+	const [showAuth, setShowAuth] = useState(false)
 	const navigate = useNavigate();
 	const navigateToUser = () => {
 		navigate('/user');
@@ -11,6 +13,23 @@ export default function Home() {
 		navigate('/admin');
 	};
 
+	const handleAuthSubmit = (e: { preventDefault: () => void; target: any; }) => {
+		// Prevent the browser from reloading the page
+		e.preventDefault();
+		
+		// Read the form data
+		const form = e.target;				
+		const formData = new FormData(form);
+		const username = formData.get('username')!.toString();
+		const password = formData.get('username')!.toString();
+
+		if (username==="admin" && password==="password") {
+			navigateToAdmin()
+		}
+	}
+	const showAuthDialog =() => {
+		setShowAuth(true)
+	}
   return (
 		<div className="App">
 			{/* {openModal && <RegisterCertification setOpenModal={setOpenModal} dId={certification.DId} setCertification={setCertification} setPrivateKey={setPrivateKey}/>} */}
@@ -19,8 +38,25 @@ export default function Home() {
 			</header>
 			<div className="App-body">
 				<div>
+					{showAuth && (
+						<div className='Auth-modal'>
+							<form method="post"  onSubmit={handleAuthSubmit} >
+								<label className='Modal-label'>
+									<h5 className='Modal-text'>UserName</h5> 
+									<input className='Modal-textField' type='text' name="username" />
+								</label>
+								<label className='Modal-label' >
+									<h5 className='Modal-text'>Password</h5>
+									<input className='Modal-textField' type="password" />
+								</label>
+								<hr className='App-border'/>
+								<button className='App-button Ok-button' type="submit"  value="Submit" >Ok</button>
+								<button className='App-button Back-button' onClick={()=>setShowAuth(false)}>Close</button>
+							</form>
+						</div>)
+					}
 					<button className='App-button User-button' onClick={navigateToUser}>User</button>
-					<button className='App-button Admin-button' onClick={navigateToAdmin}>Admin</button>
+					<button className='App-button Admin-button' onClick={showAuthDialog}>Admin</button>
 				</div>
 			</div>
 		</div>
